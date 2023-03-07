683563a7a0bcd86cefd18a5c9b0a1019ca0b80e2
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-03-07 09:57:04 +0700

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Putut Yusri Bahtiar on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
